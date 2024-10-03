2262acc9ea1e50e5c214f5d87a2d71be8a9b1f81
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-03 22:45:49 +0500

    change views.py

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from pages.form import RegisterForm
+
 
 def home_view(request):
     return render(request, 'index.html')
@@ -10,4 +12,11 @@ def login_view(request):
 
 
 def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.is_active = False
+            user.save()
     return render(request, 'user-register.html')
