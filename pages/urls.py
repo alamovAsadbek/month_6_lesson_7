ba89ffa279216c45b28fcec73f76ce279ab6a765
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-03 23:38:29 +0500

    change

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('auth/login/', views.login_view, name='login'),
-    path('auth/register/', views.register_view, name='register')
+    path('auth/register/', views.register_view, name='register'),
+    path('verify-email/', views.logout_view, name='logout'),
 ]
