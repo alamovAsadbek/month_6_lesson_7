7c201ace943e7eaedb5bca282592a6142ab9ae61
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-03 20:13:12 +0500

    add new functions

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'index.html')
+
+
+def login_view(request):
+    pass
+
+
+def register_view(request):
+    pass
