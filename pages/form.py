ae6cce7f48a25079b4c9b37b68b341f541e47507
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-03 20:52:41 +0500

    change form.py

--- pages/form.py
+++ pages/form.py
@@ -9,4 +9,7 @@ class LoginForm(forms.Form):
 
 
 class RegisterForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password']
+
