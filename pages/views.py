c47e5d8ba2ff076f09774b462b9f80cbdde0ba50
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-04 01:03:59 +0500

    change views.py

--- pages/views.py
+++ pages/views.py
@@ -77,7 +77,7 @@ def register_view(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password1'])
+            user.set_password(form.cleaned_data['password'])
             user.is_active = False
             user.save()
             send_email_verification(request, user)
