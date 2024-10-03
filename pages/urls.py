18b6ed3096bc8bd2d0ae91da42e1ec6c10da80b7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-03 20:15:16 +0500

    change user-login.html

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('auth/login/', views.login_view, name='login')
+    path('auth/login/', views.login_view, name='login'),
     path('auth/register/', views.register_view, name='register')
 ]
